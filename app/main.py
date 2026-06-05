0475506deaa1bc8b5ab26d6b178424dbc5722e12
tasbihaasim <tasbihaasim@gmail.com>
2026-06-05 19:48:25 +0200

    redis and frontend dashboard

--- app/main.py
+++ app/main.py
@@ -1,31 +1,53 @@
-from fastapi import FastAPI, HTTPException
+import redis
+import json
+from fastapi import FastAPI, HTTPException, WebSocket
 from fastapi import Request, Response
 from app.database import SessionLocal
 from app.model import WebhookEvent, DLQItem
 import hmac, hashlib, os
 from app.tasks import process_webhook_event
+import asyncio
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
+from fastapi.middleware.cors import CORSMiddleware
 
 
+
+r = redis.from_url(os.getenv("REDIS_URL"))
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.post("/webhook/{source}")
-async def get_source(source: str, request:Request):
-    secret = os.getenv("WEBHOOK_SECRET")
-    raw_body = await request.body()
-    payload = await request.json()
-    expected = hmac.new(secret.encode(), raw_body, digestmod=hashlib.sha256).hexdigest() #creates an HMAC object using your secret as the key and SHA256 as the hashing algorithm on raw_body
-    actual = request.headers.get("X-Zapier-Signature", "").split("sha256=")[-1] # retrieves the signature from the header and removes the "sha256=" prefix to get the actual signature value
-    if not hmac.compare_digest(expected, actual):
-        return {"error": "Invalid signature"}
-    process_webhook_event.delay(source, payload)
-    return Response(status_code=202)
+@app.get("/dashboard")
+async def dashboard():
+    return FileResponse("static/index.html")
 
-@app.get("/webhook/dlq_item")
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()  # opens the persistent connection with Angular
+    pubsub = r.pubsub()
+    pubsub.subscribe("webhook_events")  # subscribe to Redis channels for new webhook events and DLQ items
+    
+    while True:
+        message = pubsub.get_message()  # check if Redis published anything
+        if message and message["type"] == "message": # if there is a new message, forward it to Angular # checking the type because we only forward actual messages, not subscription confirmations
+            data = json.loads(message["data"])
+            await websocket.send_json(data)  # forward to Angular
+        await asyncio.sleep(0.01)  # small sleep to prevent this loop from consuming too much CPU when idle
+
+
+@app.get("/dlq_items")
 async def get_dlq_items():
     db = SessionLocal()
     try:
@@ -34,14 +56,14 @@ async def get_dlq_items():
     finally:
         db.close()
 
-@app.post("/retry/{dlq_item_id}")
+@app.post("/dlq/{dlq_item_id}/retry")
 async def retry_dlq_item(dlq_item_id: int):
     db = SessionLocal()
     try:
         dlq_item = db.query(DLQItem).filter(DLQItem.id == dlq_item_id).first()
-        dlq_item.retry_count += 1
         if not dlq_item:
             raise HTTPException(status_code=404, detail="DLQ item not found")
+        dlq_item.retry_count += 1
         webhook_event = db.query(WebhookEvent).filter(WebhookEvent.id == dlq_item.webhook_event_id).first()
         if not webhook_event:
             raise HTTPException(status_code=404, detail="Webhook event not found")
@@ -52,7 +74,7 @@ async def retry_dlq_item(dlq_item_id: int):
     finally:
         db.close()
 
-@app.delete("/dlq_item/{dlq_item_id}")
+@app.delete("/dlq/{dlq_item_id}")
 async def delete_dlq_item(dlq_item_id: int):
     db = SessionLocal()
     try:
@@ -64,3 +86,15 @@ async def delete_dlq_item(dlq_item_id: int):
         return {"message": "DLQ item deleted"}
     finally:
         db.close()
+
+@app.post("/ingest/{source}")
+async def get_source(source: str, request:Request):
+    secret = os.getenv("WEBHOOK_SECRET")
+    raw_body = await request.body()
+    payload = await request.json()
+    expected = hmac.new(secret.encode(), raw_body, digestmod=hashlib.sha256).hexdigest() #creates an HMAC object using your secret as the key and SHA256 as the hashing algorithm on raw_body
+    actual = request.headers.get("X-Zapier-Signature", "").split("sha256=")[-1] # retrieves the signature from the header and removes the "sha256=" prefix to get the actual signature value
+    if not hmac.compare_digest(expected, actual):
+        return {"error": "Invalid signature"}
+    process_webhook_event.delay(source, payload)
+    return Response(status_code=202)
--- app/tasks.py
+++ app/tasks.py
@@ -1,10 +1,14 @@
 from celery import Celery
+import redis
+import json
 from kombu import Queue
 from app.model import WebhookEvent, DLQItem
 from app.database import SessionLocal
 import os
 from dotenv import load_dotenv
 load_dotenv()
+
+r = redis.from_url(os.getenv("REDIS_URL"))
 
 celery_app = Celery(
     "tasks",
@@ -28,19 +32,21 @@ def process_webhook_event(self, source: str, payload: dict):
     webhook_event = WebhookEvent(source=source, payload=payload, status="received")
     db = SessionLocal()
     try:
-        #raise Exception("forced failure")
         db.add(webhook_event)
         db.commit()
         db.refresh(webhook_event)
+        #raise Exception("forced failure") # simulate a failure to trigger retries and eventually the DLQ
+        r.publish("webhook_events", json.dumps({"id": webhook_event.id, "source": source, "payload": payload, "status": "received"}))
     except Exception as e:
         db.rollback()
         if self.request.retries >= self.max_retries:
             # this is the last attempt, save to DLQ before giving up
             process_dlq_item.delay(webhook_event.id, source, payload, str(e))
+
         raise self.retry(exc=e, countdown=2 ** self.request.retries)
     finally:
         db.close()
-
+        
 @celery_app.task(bind=True, queue="dlq")
 def process_dlq_item(self, webhook_event_id: int, source: str, payload: dict, error_message: str):
     # This function should recieve source, payload and error message
@@ -53,6 +59,7 @@ def process_dlq_item(self, webhook_event_id: int, source: str, payload: dict, er
         db.add(dlq_item)
         db.commit()
         db.refresh(dlq_item)
+        r.publish("webhook_events", json.dumps({"id": dlq_item.id, "source": source, "payload": payload, "error_message": error_message, "status": "failed"}))
     except Exception as e:
         db.rollback()
         print(f"Failed to save DLQ item: {e}")
